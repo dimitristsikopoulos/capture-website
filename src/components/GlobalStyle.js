d2b9b8ea2f407425175a1ee7b602544323c5da8f
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-11-19 21:52:50 +0200

    23. Mobile Friendly

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -4,17 +4,22 @@ const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    box-sizing: border-box
 }
-
+html{
+    @media (max-width: 1700px){
+        font-size: 75%;
+    }
+    
+}
 body{
     background: #1b1b1b;
     font-family: 'Inter', sans-serif;
-    overflow-x:hidden;
+    overflow-x: hidden;
 }
 button{
     font-weight: bold;
-    font-size: 1.1rem;
+    font-size: 1.1.rem;
     cursor: pointer;
     padding: 1rem 2rem;
     border: 3px solid #23d997;
@@ -26,9 +31,8 @@ button{
         background-color: #23d997;
         color: white;
     }
-    
 }
-h2{
+    h2{
         font-weight: lighter;
         font-size: 4rem;
     }
@@ -36,16 +40,16 @@ h2{
         color: white;
     }
     h4{
-        font-size: 2rem;
         font-weight: bold;
-        
+        font-size: 2rem;
+       
     }
     a{
-        font-size:1.1rem ;
+        font-size: 1.1rem;
     }
     span{
-        font-weight:bold;
-        color: #23d997;
+        font-weight: bold;
+        color:#23d997;
     }
     p{
         padding: 3rem 0rem;
